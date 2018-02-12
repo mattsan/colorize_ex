4f6dc38215e38a03c5751b14a5115b96a2582271
Eiji MATSUMOTO <e.mattsan@gmail.com>
2018-02-12 13:39:34 +0900

    add colization module

--- lib/colorize.ex
+++ lib/colorize.ex
@@ -1,18 +1,74 @@
 defmodule Colorize do
   @moduledoc """
-  Documentation for Colorize.
+  string olorization sigil
+
+  see https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+
+  - `k` - black
+  - `r` - red
+  - `g` - green
+  - `y` - yellow
+  - `b` - blue
+  - `m` - magenta
+  - `c` - cyan
+  - `w` - w
+
+  A option character is capitalized, the string is put bright color.
+
+  ```
+  ~l"white"w # white
+  ~l"white"W # bright white
+  ```
+
+  A second option defines background color.
+
+  ```
+  ~l"black in white"wk # string is white, background is black
+  ```
   """
+
+  @color_table %{
+    ?k => 30, ?K => 90,
+    ?r => 31, ?R => 91,
+    ?g => 32, ?G => 92,
+    ?y => 33, ?Y => 93,
+    ?b => 34, ?B => 94,
+    ?m => 35, ?M => 95,
+    ?c => 36, ?C => 96,
+    ?w => 37, ?W => 97
+  }
+
+  @colors Map.keys(@color_table)
 
   @doc """
-  Hello world.
+  colorize string
 
-  ## Examples
+  ## example
 
-      iex> Colorize.hello
-      :world
-
+  ```
+  iex> import Colorize
+  iex> ~l"red"r
+  "\e[31mred\e[m"
+  iex> ~l"green"g
+  "\e[32mgreen\e[m"
+  iex> ~l"blue"b
+  "\e[34mblue\e[m"
+  iex>
+  ```
   """
-  def hello do
-    :world
+  def sigil_l(s, [color]) when color in @colors do
+    colorize(s, @color_table[color])
+  end
+
+  def sigil_l(s, [color, background]) when color in @colors and background in @colors do
+    colorize(s, @color_table[color], @color_table[background])
+  end
+
+  defp colorize(s, code) do
+    "\x1b[#{code}m#{s}\x1b[m"
+  end
+
+  defp colorize(s, front, back) do
+    "\x1b[#{front};#{back + 10}m#{s}\x1b[m"
   end
 end
--- test/colorize_test.exs
+++ test/colorize_test.exs
@@ -2,7 +2,85 @@ defmodule ColorizeTest do
   use ExUnit.Case
   doctest Colorize
 
-  test "greets the world" do
-    assert Colorize.hello() == :world
+  import Colorize
+
+  test "black" do
+    assert ~l"black"k == "\x1b[30mblack\x1b[m"
+    assert ~l"black"K == "\x1b[90mblack\x1b[m"
+  end
+
+  test "red" do
+    assert ~l"red"r == "\x1b[31mred\x1b[m"
+    assert ~l"red"R == "\x1b[91mred\x1b[m"
+  end
+
+  test "green" do
+    assert ~l"green"g == "\x1b[32mgreen\x1b[m"
+    assert ~l"green"G == "\x1b[92mgreen\x1b[m"
+  end
+
+  test "yellow" do
+    assert ~l"yellow"y == "\x1b[33myellow\x1b[m"
+    assert ~l"yellow"Y == "\x1b[93myellow\x1b[m"
+  end
+
+  test "blue" do
+    assert ~l"blue"b == "\x1b[34mblue\x1b[m"
+    assert ~l"blue"B == "\x1b[94mblue\x1b[m"
+  end
+
+  test "magenta" do
+    assert ~l"magenta"m == "\x1b[35mmagenta\x1b[m"
+    assert ~l"magenta"M == "\x1b[95mmagenta\x1b[m"
+  end
+
+  test "cyan" do
+    assert ~l"cyan"c == "\x1b[36mcyan\x1b[m"
+    assert ~l"cyan"C == "\x1b[96mcyan\x1b[m"
+  end
+
+  test "white" do
+    assert ~l"white"w == "\x1b[37mwhite\x1b[m"
+    assert ~l"white"W == "\x1b[97mwhite\x1b[m"
+  end
+
+  test "black on white" do
+    assert ~l"black"kw == "\x1b[30;47mblack\x1b[m"
+    assert ~l"black"KW == "\x1b[90;107mblack\x1b[m"
+  end
+
+  test "red on black" do
+    assert ~l"red"rk == "\x1b[31;40mred\x1b[m"
+    assert ~l"red"RK == "\x1b[91;100mred\x1b[m"
+  end
+
+  test "green on red" do
+    assert ~l"green"gr == "\x1b[32;41mgreen\x1b[m"
+    assert ~l"green"GR == "\x1b[92;101mgreen\x1b[m"
+  end
+
+  test "yellow on green" do
+    assert ~l"yellow"yg == "\x1b[33;42myellow\x1b[m"
+    assert ~l"yellow"YG == "\x1b[93;102myellow\x1b[m"
+  end
+
+  test "blue on yellow" do
+    assert ~l"blue"by == "\x1b[34;43mblue\x1b[m"
+    assert ~l"blue"BY == "\x1b[94;103mblue\x1b[m"
+  end
+
+  test "magenta on blue" do
+    assert ~l"magenta"mb == "\x1b[35;44mmagenta\x1b[m"
+    assert ~l"magenta"MB == "\x1b[95;104mmagenta\x1b[m"
+  end
+
+  test "cyan on magenta" do
+    assert ~l"cyan"cm == "\x1b[36;45mcyan\x1b[m"
+    assert ~l"cyan"CM == "\x1b[96;105mcyan\x1b[m"
+  end
+
+  test "white on cyan" do
+    assert ~l"white"wc == "\x1b[37;46mwhite\x1b[m"
+    assert ~l"white"WC == "\x1b[97;106mwhite\x1b[m"
   end
 end
