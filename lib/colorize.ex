d357808ade5e79658b6131d7ad8d82bf88c27430
Eiji MATSUMOTO <e.mattsan@gmail.com>
2018-02-12 12:44:35 +0900

    initial commit

---
+++ lib/colorize.ex
@@ -0,0 +1,18 @@
+defmodule Colorize do
+  @moduledoc """
+  Documentation for Colorize.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Colorize.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/colorize_test.exs
@@ -0,0 +1,8 @@
+defmodule ColorizeTest do
+  use ExUnit.Case
+  doctest Colorize
+
+  test "greets the world" do
+    assert Colorize.hello() == :world
+  end
+end
